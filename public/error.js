fd7b233e8431b215c51b9cecea413265fd610850
Josh S <joshs2006@proton.me>
2023-05-27 14:10:16 -0400

    Add files via upload

---
+++ public/error.js
@@ -0,0 +1,20 @@
+"use strict";
+const error = document.getElementById("rv-error");
+const errorCode = document.getElementById("rv-error-code");
+const registerButton = document.getElementById("rv-register-sw");
+
+if (location.pathname.startsWith(__uv$config.prefix)) {
+  error.textContent = "Error: The seuvice worker is not registered.";
+  registerButton.classList.add("show");
+}
+
+registerButton.addEventListener("click", async () => {
+  try {
+    await registerSW();
+    location.reload();
+  } catch (err) {
+    error.textContent = "Failed to register seuvice worker.";
+    errorCode.textContent = err.toString();
+    registerButton.classList.remove("show");
+  }
+});
